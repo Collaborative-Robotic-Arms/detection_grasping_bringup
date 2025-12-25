63e182444225de989fb0c5b5a6b9c90091c3dbb3
Mohamed Alaa <mohammed.alaa200080@gmail.com>
2025-12-25 19:29:20 +0200

    add the bringup launch file

---
+++ launch/detection_grasping_bringup.launch.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import os, sys, xacro, yaml
+from ament_index_python.packages import get_package_share_directory, PackageNotFoundError
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, TimerAction, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+import yaml
+
+
+# LOAD FILE:
+def load_file(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return file.read()
+    except EnvironmentError:
+        return None
+        
+# LOAD YAML:
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    print("absolute path: ",absolute_file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        return None
+def generate_launch_description():
+    LD = LaunchDescription()
+    
+    grasping_node = Node(
+        package="brick_grasping_model",
+        executable="brick_grasping_node.py",
+        output="screen",
+        parameters=[]
+    )
+
+    detection_node = Node(
+        package="brick_detection",
+        executable="yolo_detector",
+        output="screen",
+        parameters=[]
+    )
+
+
+
+
+    
+    # Add nodes to the launch description:
+    LD.add_action(grasping_node)
+    LD.add_action(detection_node)
+    # LD.add_action(static_tf)
+    
+    return LD
+
+if __name__ == '__main__':
+    ld = generate_launch_description()
